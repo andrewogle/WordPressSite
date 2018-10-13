65af7eaa4f399cfec825413fdc79de36c61a162a
Andrew <andrewryanogle@gmail.com>
2018-10-13 08:39:01 -0700

    finally enqueued a custom style sheet for front-page

--- functions.php
+++ functions.php
@@ -1,20 +1,19 @@
 <?php
-add_action('wp_enqueue_style', 'my_portfolio_styles' );
-  if (is_front_page()){
-	wp_enqueue_style('front-page',  get_stylesheet_directory().'resources/front-page.css');
 
-	function my_portfolio_styles(){
-
-		wp_enqueue_style('resources', get_stylesheet_directory_url('front-page.css'),array('front-page.css'));
-	}
+add_action('wp_enqueue_scripts', 'my_portfolio_styles');
+function my_portfolio_styles()
+{
+  if (is_front_page()) {
+    wp_enqueue_style('front-page', get_stylesheet_directory_uri() . '/resources/front-page.css', array(), false, 'screen');
+  }
 }
 
 
 
 	// Navigation Menus
 
-	register_nav_menus(array(
-		'primary' => __('Primary Menu'),
-		'footer' => __('Footer Menu'),
-    ));
+register_nav_menus(array(
+  'primary' => __('Primary Menu'),
+  'footer' => __('Footer Menu'),
+));
 ?>
